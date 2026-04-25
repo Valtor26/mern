93a104cbb30b3746c4e877e0aeef05bda2d1d032
abhiiks26 <abhishekabhi2618@gmail.com>
2026-04-25 13:02:11 +0530

    Control Statements revision

---
+++ control-statements/script.js
@@ -0,0 +1,39 @@
+// if, else, else if
+
+//switch case
+
+// let a = 11;
+
+// switch (a) {
+//   case 1:
+//     console.log("One");
+//     break;
+//   case 2:
+//     console.log("Two");
+//     break;
+
+//   default:
+//     console.log("Different");
+//     break;
+// }
+
+// Early return pattern
+
+// function canVote(age) {
+//   if (age >= 18) return "YES";
+//   else return "NO";
+// }
+
+// let res = canVote(11);
+
+// function getGrade(score) {
+//   if (score >= 90 && score <= 100) return "A+";
+//   else if (score >= 80 && score <= 89) return "A";
+//   else if (score >= 70 && score <= 79) return "B";
+//   else if (score >= 60 && score <= 69) return "C";
+//   else if (score >= 33 && score <= 59) return "D";
+//   else if (score >= 0 && score <= 32) return "Fail";
+//   else {
+//     return "Invalid Marks";
+//   }
+// }
