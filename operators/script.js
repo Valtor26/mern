7d08fd471c7018b85855087e88088122fdf29ae6
abhiiks26 <abhishekoggy26@gmail.com>
2026-04-25 13:09:17 +0530

    JS Operators revision

---
+++ operators/script.js
@@ -0,0 +1,9 @@
+// Arithematic, Comparison, Logical, Assignment, Unary, Ternary
+
+// Arithematic -> +,-,*,/,%,**(2 ** 3 ----> 2^3)
+// Comparison -> ==,===,!=,!==,<,>,<=,>=
+// Logical -> AND &&, NOT !, OR ||
+// Assignment -> =,+=,-=,*=,/=,%=
+// unary -> applied on single value + (+"5" --> converts to number -> 5) - ! typeof ++ --
+// Ternary -> condition ? if true : if false; ---->
+// eg. age >= 18 ? console.log("Can vote") : console.log("Cannot vote")
