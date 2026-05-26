d610120600427df77d328e3fa6b199af8a016021
abhiiks26 <abhishekoggy26@gmail.com>
2026-05-26 12:59:56 +0530

    dynamic card using form inputs

--- events/script.js
+++ events/script.js
@@ -80,3 +80,48 @@
 //   }
 // });
 //-------------------------------------------------------->
+// Form submit
+// preventDefault() ---> stops page from reloading when form is submitted
+
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let body = document.querySelector("body");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let profile = document.createElement("div");
+  profile.classList.add("profile");
+
+  let img = document.createElement("img");
+  img.setAttribute(
+    "src",
+    "https://images.unsplash.com/photo-1773622597179-44ea0fecd5b4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D",
+  );
+
+  let h3 = document.createElement("h3");
+  h3.textContent = inputs[0].value;
+
+  let h5 = document.createElement("h5");
+  h5.textContent = inputs[1].value;
+
+  let p = document.createElement("p");
+  p.textContent = "Software Engineer with internship experience";
+
+  profile.appendChild(img);
+  card.appendChild(profile);
+  card.appendChild(h3);
+  card.appendChild(h5);
+  card.appendChild(p);
+
+  body.appendChild(card);
+
+  inputs.forEach((val) => {
+    if (val.type != "submit") {
+      val.value = "";
+    }
+  });
+});
