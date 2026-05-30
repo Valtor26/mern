0c8061fa152fe22af4f5c43e991fc0f24f2e6b4e
abhiiks26 <abhishekoggy26@gmail.com>
2026-05-30 12:51:24 +0530

    timers and intervals

---
+++ timers-intervals/script.js
@@ -0,0 +1,37 @@
+// setTimeout(func(){},time);
+// 1 sec = 1000 mSec
+// setTimeout is used to perform an action after some amount of time
+
+// setTimeout(() => {
+//   console.log("Hello");
+// }, 3000); //runs after 3 sec
+//----------------------------------->
+
+// setInterval(func(){},time);
+// setInterval runs simultaneously after some amount of time, where as setInterval runs only once
+
+// setInterval(() => {
+//   console.log("Hello");
+// }, 3000);
+//---------------------------------->
+
+// clearTimeout(variable)
+// used to remove the setTimeout, for that we need to store the setTimeout in a variable
+
+// let tm = setTimeout(() => {
+//   console.log("Hiii");
+// }, 3000);
+
+// clearTimeout(tm);
+//---------------------------------->
+
+// clearInterval(variable)
+// same as clearTimeout, used to remove any setInterval()
+
+// let iv = setInterval(() => {
+//   console.log("Hiiu");
+// }, 3000);
+
+// clearInterval(iv);
+//--------------------------------------->
+
