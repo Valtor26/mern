917d264939423a621ca761a5e8584cfd62e5e03e
abhiiks26 <abhishekoggy26@gmail.com>
2026-06-03 13:34:00 +0530

    scope

---
+++ scope/script.js
@@ -0,0 +1,14 @@
+// scope: area until which i can access the created variables and functions
+
+// functional scope: only accessible inside functions
+// global: can be accessed anywhere
+// block: only accesible within the curly braces
+
+// function abcd() {
+//   var a = 12;
+// }
+
+// console.log(a); //script.js:11 Uncaught ReferenceError: a is not defined
+// at script.js:11:13
+
+
