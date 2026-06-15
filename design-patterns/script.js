bdf48a03dc149c719f9eb7fa7a13e34cd54d57ca
Valtor26 <abhishekoggy26@gmail.com>
2026-06-15 13:22:09 +0530

    Design patterns updated

--- design-patterns/script.js
+++ design-patterns/script.js
@@ -74,28 +74,71 @@
 
 // this pattern is useful when we want multiple objects of the same type like users, products, tasks etc..
 
-function createProducts(name,price){
-    let stock = 10;
+// function createProducts(name,price){
+//     let stock = 10;
 
-    return {
-        name,
-        price,
-        buy(qty){
-            if(qty <= stock){
-                stock -= qty;
-                console.log(`${qty} items booked - ${stock} items left`);
-            }
-            else{
-                if(stock === 0) console.error(`OOPS - Out of stock`);
-                else console.error(`OOPS - We only have ${stock} items`);
-            }
-        },
-        refill(qty){
-            stock += qty;
-            console.log(`refilled the stock - ${stock} available`);
-        }
-    }
-}
+//     return {
+//         name,
+//         price,
+//         buy(qty){
+//             if(qty <= stock){
+//                 stock -= qty;
+//                 console.log(`${qty} items booked - ${stock} items left`);
+//             }
+//             else{
+//                 if(stock === 0) console.error(`OOPS - Out of stock`);
+//                 else console.error(`OOPS - We only have ${stock} items`);
+//             }
+//         },
+//         refill(qty){
+//             stock += qty;
+//             console.log(`refilled the stock - ${stock} available`);
+//         }
+//     }
+// }
 
-let iphone = createProducts("iphone",50000);
-let samsung = createProducts("samsung",35000);
+// let iphone = createProducts("iphone",50000);
+// let samsung = createProducts("samsung",35000);
+
+
+//----------------------------------------------------------------->
+
+// Observer pattern: The Observer Pattern in JavaScript is a behavioral design pattern where one object (Subject) notifies multiple dependent objects (Observers) automatically whenever its state changes.
+
+// class Dept{
+//     constructor(){
+//         this.students = [];
+//     }
+//     enroll(student){
+//         this.students.push(student);
+//         console.log(`${student.name} you have enrolled`)
+//     }
+//     notify(circular){
+//         this.students.forEach((stud) =>{
+//             stud.update(circular);
+//         })
+//     }
+// }
+
+// class Student{
+//     constructor(name){
+//         this.name = name;
+//     }
+
+//     update(message){
+//         console.log(`${this.name} your update: ${message}`);
+//     }
+// }
+
+// let cse = new Dept();
+// let me = new Dept();
+
+// let stud1 = new Student("Abhishek");
+// let stud2 = new Student("Vishal");
+// let stud3 = new Student("Rahul");
+
+// cse.enroll(stud1);
+// cse.notify("Exam from 20th June")
+// me.enroll(stud2);
+// me.enroll(stud3);
+// me.notify("Exam from 30th June");
