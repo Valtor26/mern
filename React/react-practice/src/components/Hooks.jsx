b31c2dc57c393f26d717800484596a9753c79dcc
Valtor26 <abhishekoggy26@gmail.com>
2026-06-25 13:01:35 +0530

    intro to hooks

---
+++ React/react-practice/src/components/Hooks.jsx
@@ -0,0 +1,7 @@
+// useState - its used to manage state
+// useEffect - used to handle side effects, like API call, DOM manipulation, event listener
+// useContext - used to consume global state without prop drilling, Share data across components without passing props through every intermediate component.
+// useReducer - used to manage complex state (small version of Redux)
+// useRef - Store a value without causing a re-render, or access a DOM element
+// useMemo - Remember an expensive calculation so it isn't repeated unnecessarily
+// useCallback - Remember a function so React doesn't recreate it on every render
