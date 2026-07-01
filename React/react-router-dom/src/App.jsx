0f08562c23b776cc80d5ffec6de070fbdb607f8e
Valtor26 <abhishekoggy26@gmail.com>
2026-07-01 13:20:55 +0530

    react router basic setup

---
+++ React/react-router-dom/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ React/react-router-dom/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to='/contact'>Contact</Link>
+        <br />
+        <Link to='/about'>About</Link>
+    </div>
+  )
+}
+
+export default Navbar
