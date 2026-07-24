ee562dd431da9249adb43853553dd1320c6dc16e
Valtor26 <abhishekoggy26@gmail.com>
2026-07-24 13:18:53 +0530

    node setup and installing packages

---
+++ backend/setup/index.js
@@ -0,0 +1,10 @@
+const catme = require('cat-me');
+
+console.log(catme())
+
+
+// all the code of the installed package will be stored in node modules
+
+// package.json --> it maitains all the dependencies package of your code
+
+// package-lock.json ---> it maintains all the dependency chain for every package
