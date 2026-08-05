e5c9800383abec2ad268169d64df627d000daa39
Abhishek <100672043+Valtor26@users.noreply.github.com>
2026-08-05 13:04:43 +0530

    Delete backend/imageFeed directory

--- backend/imageFeed/src/models/post.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const postSchema = new mongoose.Schema({
-    image:String,
-    caption:String
-})
-
-const postModel = mongoose.model("post",postSchema) //(collection name, schema name)
-module.exports = postModel
