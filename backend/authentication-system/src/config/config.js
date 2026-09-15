b4e948ed88984e9e72595bfb7b1f59a7d14c1d1b
Valtor26 <abhishekoggy26@gmail.com>
2026-09-15 12:50:27 +0530

    Registering user and db setup

--- backend/authentication-system/src/config/config.js
+++ backend/authentication-system/src/config/config.js
@@ -5,8 +5,13 @@ if (!process.env.MONGO_URI) {
     throw new Error("Please set the MONGO_URI environment variable");
 }
 
+if (!process.env.JWT_SECRET) {
+    throw new Error("Please set the JWT_SECRET environment variable");
+}
+
 const config = {
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET
 }
 
 export default config;
